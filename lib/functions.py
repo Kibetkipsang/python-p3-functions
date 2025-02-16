b7f50a28e5bf2011d839021395ec045300bd0d54
Kibet Kipsang <denniskipsang2000@gmail.com>
2025-02-16 12:24:35 +0300

    functions.py

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+
+greet('Kibet')
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+
+greet_with_default()
 
 def add(num1, num2):
-    pass
+    print('num 1' + 'num2')
+    return num1 + num2
+
+add(4, 6)
 
 def halve(number):
-    pass
+    print(number / 2)
+    return number / 2
+
+halve(8)
